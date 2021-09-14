75e242dd32afd54a59c9828de69fd0e0ded58cb0
jihanradhana <jihanrmdhn@gmail.com>
2021-09-14 13:12:50 +0700

    Adding Comments

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM  : 21537144008
+Nama : Jihan Ramadhana
+*/
 public class Hello {
     public static void main(String[] args) {
+        // Untuk menampilkan teks
         System.out.println("Hello, world!");
     }
 }
