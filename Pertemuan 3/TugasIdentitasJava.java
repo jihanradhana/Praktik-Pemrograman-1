cf659f2f61a2d291802569dd3383551328f71a15
jihanradhana <jihanrmdhn@gmail.com>
2021-09-14 19:18:35 +0700

    Tugas Pertemuan 3 (Java)

---
+++ Pertemuan 3/TugasIdentitasJava.java
@@ -0,0 +1,25 @@
+public class TugasIdentitasJava {
+    public static void main(String[] args) {
+        String namaLengkap = "Jihan Ramadhana";
+        String nomorIndukMahasiswa = "21537144008";
+        int tahunAngkatan = 2021;
+        String programStudi = "S1 Teknologi Informasi";
+        String jurusan = "Jurusan Pendidikan Teknik Elektronika dan Informatika (JPTEI)";
+        String fakultas = "Fakultas Teknik";
+        double ipkHarapan = 3.5;
+
+        System.out.println("Pada tugas kali ini, saya akan memperkenalkan diri saya.");
+        System.out.println("Berikut adalah identitas diri saya :");
+        System.out.println();
+        System.out.println("Nama Lengkap  : " + namaLengkap);
+        System.out.println("NIM           : " + nomorIndukMahasiswa);
+        System.out.println("Angkatan      : " + tahunAngkatan);
+        System.out.println("Program Studi : " + programStudi);
+        System.out.println("Jurusan       : " + jurusan);
+        System.out.println("Fakultas      : " + fakultas);
+        System.out.println("Nama PTN      : Universitas Negeri Yogyakarta");
+        System.out.println("Target IPK    : " + ipkHarapan);
+        System.out.println();
+        System.out.println("Sekian dari saya, mohon maaf jika ada kesalahan, terima kasih.");
+    }
+}
