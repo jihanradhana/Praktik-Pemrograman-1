e3e1489ccbed5ffc192323ecc26396182cabf207
jihanradhana <jihanrmdhn@gmail.com>
2021-09-14 19:48:16 +0700

    Tugas Pertemuan 3 Python (belum fix)

---
+++ Pertemuan 3/TugasIdentitasPython.py
@@ -0,0 +1,18 @@
+nama = "Jihan Ramadhana"
+nim = 21537144008
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "Jurusan Pendidikan Teknik Elektronika dan Informatika (JPTEI)"
+fakultas = "Fakultas Teknik Universitas Negeri Yogyakarta"
+
+
+print("Pada tugas kali ini, saya akan memperkenalkan diri saya")
+print("Berikut adalah identitas diri saya : ")
+print()
+print("Nama Lengkap : " + nama)
+print(nim)
+print(angkatan)
+print("Program Studi : " + programStudi)
+print("Jurusan : " + jurusan)
+print("Fakultas : " + fakultas)
+print()
