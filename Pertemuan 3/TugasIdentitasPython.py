ff99533a31392f9bbd2b4e93ce069d5721e538a2
jihanradhana <jihanrmdhn@gmail.com>
2021-09-15 12:41:04 +0700

    Tugas Python

--- Pertemuan 3/TugasIdentitasPython.py
+++ Pertemuan 3/TugasIdentitasPython.py
@@ -6,13 +6,12 @@ jurusan = "Jurusan Pendidikan Teknik Elektronika dan Informatika (JPTEI)"
 fakultas = "Fakultas Teknik Universitas Negeri Yogyakarta"
 
 
-print("Pada tugas kali ini, saya akan memperkenalkan diri saya")
+print("Pada tugas kali ini, saya akan memperkenalkan diri saya.")
 print("Berikut adalah identitas diri saya : ")
-print()
-print("Nama Lengkap : " + nama)
-print(nim)
-print(angkatan)
-print("Program Studi : " + programStudi)
-print("Jurusan : " + jurusan)
-print("Fakultas : " + fakultas)
-print()
+print("Nama Lengkap          : " + nama)
+print("Nomor Induk Mahasiswa : " + str(nim))
+print("Tahun Angkatan        : " + str(angkatan))
+print("Program Studi         : " + programStudi)
+print("Jurusan               : " + jurusan)
+print("Fakultas              : " + fakultas)
+print("Sekian dari saya, mohon maaf jika ada kesalahan, terima kasih.")
