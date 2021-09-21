586e5ed1c3137319d8c416916dd6594994df6ea6
jihanradhana <jihanrmdhn@gmail.com>
2021-09-21 23:02:17 +0700

    Tugas Pertemuan Ke-Empat

--- Pertemuan 4/TugasJava.java
+++ Pertemuan 4/TugasJava.java
@@ -4,35 +4,35 @@ public class TugasJava {
     public static void main(String[] args) {
         Scanner j = new Scanner(System.in);
         
-        System.out.print("Masukkan nilai dari x = "); 
+        System.out.print("Masukkan nilai dari x : "); 
         double x = j.nextDouble();
 
-        System.out.print("Masukkan nilai dari y = ");
+        System.out.print("Masukkan nilai dari y : ");
         double y = j.nextDouble();
 
-        System.out.println("Assalamu'alaikum warahmatullahi wabarokatuh"); //pembuka
-        System.out.print("Pada tugas kali ini, saya akan membuat suatu program yang bisa membaca dua input nilai, "); 
-        System.out.println("dan juga menampilkan hasil dari penjumlahan, pengurangan, perkalian, pembagian serta modulus dari kedua bilangan tersebut.");
-        System.out.println();
-        System.out.println("Berikut adalah hasil operasi dari dua bilangan yang telah dimasukkan.");
+            System.out.println("Assalamu'alaikum warahmatullahi wabarokatuh"); //pembuka
+            System.out.print("Pada tugas kali ini, saya akan membuat suatu Program Java yang bisa membaca dua input nilai, "); 
+            System.out.println("dan juga menampilkan hasil dari penjumlahan, pengurangan, perkalian, pembagian serta modulus dari kedua bilangan tersebut.");
+            System.out.println();
+            System.out.println("Berikut adalah hasil operasi dari dua bilangan yang telah dimasukkan.");
 
-        double hasilTambah = x + y; //pertambahan
-        System.out.println("Pertambahan dari nilai x dan y akan menghasilkan nilai sebesar " + hasilTambah);
+            double hasilTambah = x + y; //pertambahan
+            System.out.println("Pertambahan dari nilai x dan y akan menghasilkan nilai sebesar " + hasilTambah);
 
-        double hasilKurang = x - y; //pengurangan
-        System.out.println("Pengurangan dari nilai x dan y akan menghasilkan nilai sebesar " + hasilKurang);
+            double hasilKurang = x - y; //pengurangan
+            System.out.println("Pengurangan dari nilai x dan y akan menghasilkan nilai sebesar " + hasilKurang);
 
-        double hasilKali = x * y; //perkalian
-        System.out.println("Perkalian dari nilai x dan y akan menghasilkan nilai sebesar " + hasilKali);
+            double hasilKali = x * y; //perkalian
+            System.out.println("Perkalian dari nilai x dan y akan menghasilkan nilai sebesar " + hasilKali);
 
-        double hasilBagi = x / y; //pembagian
-        System.out.println("Pembagian dari nilai x dan y akan menghasilkan nilai sebesar " + hasilBagi);
+            double hasilBagi = x / y; //pembagian
+            System.out.println("Pembagian dari nilai x dan y akan menghasilkan nilai sebesar " + hasilBagi);
 
-        double hasilModulus = x % y; //modulus
-        System.out.println("Modulus dari x dan y adalah " +hasilModulus);
+            double hasilModulus = x % y; //modulus
+            System.out.println("Modulus dari x dan y adalah " +hasilModulus);
 
-        System.out.println(); //penutup
-        System.out.println("Sekian tugas saya kali ini, mohon maaf jika ada kesalahan, terima kasih.");
-        System.out.println("Wassalamu'alaikum warahmatullahi wabarokatuh");
+            System.out.println(); //penutup
+            System.out.println("Sekian untuk tugas saya kali ini, mohon maaf jika ada kesalahan, terima kasih.");
+            System.out.println("Wassalamu'alaikum warahmatullahi wabarokatuh");
     }
 }
--- Pertemuan 4/TugasPython.py
+++ Pertemuan 4/TugasPython.py
@@ -1,17 +1,24 @@
 y = input ("Masukkan nilai dari variabel y yang ingin dioperasikan : ")
+z = input ("Masukkan nilai dari variabel x yang ingin dioperasikan : ")
 
-z = input ("Masukkan nilai dari variabel x yang ingin dioperasikan :")
+print("Assalamu'alaikum warahmatullahi wabarokatuh") #pembuka
+print("Pada tugas kali ini, saya akan membuat suatu Program Python yang bisa membaca dua input nilai", end=" ")
+print("dan juga menampilkan hasil dari penjumlahan, pengurangan, perkalian, pembagian serta modulus dari kedua bilangan tersebut.")
 
+hasilTambah = y + z #pertambahan
+print("Hasil pertambahan = " + str(hasilTambah)) 
 
-hasilTambah = y + z
-hasilKurang = (int(y) - int (z))
-hasilKali = (int(y) * int (z))
-hasilBagi = (int(y) / int (z))
-hasilModulus = (int(y) % int (z))
+hasilKurang = (int(y) - int (z)) #pengurangan
+print("Hasil pengurangan = " + str(hasilKurang)) 
 
-
-print("Hasil pertambahan = " + str(hasilTambah))
-print("Hasil pengurangan = " + str(hasilKurang))
+hasilKali = (int(y) * int (z)) #perkalian
 print("Hasil perkalian = " + str(hasilKali))
-print("Hasil pembagian = " + str(hasilBagi))
-print("Hasil modulus = " + str(hasilModulus))
+
+hasilBagi = (int(y) / int (z)) #pembagian
+print("Hasil pembagian = " + str(hasilBagi)) 
+
+hasilModulus = (int(y) % int (z)) #modulus
+print("Hasil modulus = " + str(hasilModulus)) 
+
+print("Sekian untuk tugas saya kali ini, mohon maaf jika ada kesalahan, terima kasih.") #penutup
+print("Wassalamu'alaikum warahmatullahi wabarokatuh")
