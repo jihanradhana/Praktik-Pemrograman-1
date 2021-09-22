30270c311d5bc5bd9c7bb78e4f000e5d44c69a4d
jihanradhana <jihanrmdhn@gmail.com>
2021-09-22 17:47:54 +0700

    Update Tugas Pertemuan Keempat

--- Pertemuan 4/TugasPython.py
+++ Pertemuan 4/TugasPython.py
@@ -1,24 +1,26 @@
-y = input ("Masukkan nilai dari variabel y yang ingin dioperasikan : ")
-z = input ("Masukkan nilai dari variabel x yang ingin dioperasikan : ")
-
 print("Assalamu'alaikum warahmatullahi wabarokatuh") #pembuka
 print("Pada tugas kali ini, saya akan membuat suatu Program Python yang bisa membaca dua input nilai", end=" ")
 print("dan juga menampilkan hasil dari penjumlahan, pengurangan, perkalian, pembagian serta modulus dari kedua bilangan tersebut.")
+print("Berikut adalah hasil dari program saya.")
+
+y = input ("Masukkan nilai dari variabel y yang ingin dioperasikan : ")
+z = input ("Masukkan nilai dari variabel x yang ingin dioperasikan : ")
+
 
 hasilTambah = y + z #pertambahan
-print("Hasil pertambahan = " + str(hasilTambah)) 
+print("Pertambahan dari x dan y akan menghasilkan nilai sebesar " + str(hasilTambah)) 
 
 hasilKurang = (int(y) - int (z)) #pengurangan
-print("Hasil pengurangan = " + str(hasilKurang)) 
+print("Pengurangan dari x dan y akan menghasilkan nilai sebesar " + str(hasilKurang)) 
 
 hasilKali = (int(y) * int (z)) #perkalian
-print("Hasil perkalian = " + str(hasilKali))
+print("Perkalian dari x dan y akan menghasilkan nilai sebesar " + str(hasilKali))
 
 hasilBagi = (int(y) / int (z)) #pembagian
-print("Hasil pembagian = " + str(hasilBagi)) 
+print("Pembagian dari x dan y akan menghasilkan nilai sebesar " + str(hasilBagi)) 
 
 hasilModulus = (int(y) % int (z)) #modulus
-print("Hasil modulus = " + str(hasilModulus)) 
+print("Hasil modulus dari nilai x dan y adalah " + str(hasilModulus)) 
 
 print("Sekian untuk tugas saya kali ini, mohon maaf jika ada kesalahan, terima kasih.") #penutup
 print("Wassalamu'alaikum warahmatullahi wabarokatuh")
