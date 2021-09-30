94f5c04a347952099207dc2db4a34c65133e0572
jihanradhana <jihanrmdhn@gmail.com>
2021-09-30 19:20:52 +0700

    Tugas Lima Update on python

---
+++ Pertemuan 5/TugasLima.py
@@ -0,0 +1,39 @@
+print("Silahkan masukkan IPK Anda di bawah dengan format dua angka di belakang koma (contoh : 3.71)", end=" ")
+print("dan waktu lama anda menempuh pembelajaran untuk melihat predikat yang berhasil anda capai.")
+
+namaLengkap = input("Nama Lengkap          : ")
+
+nim = input("Nomor Induk Mahasiswa : ")
+
+ipk = input("IPK                   : ")
+ipk = float(ipk)
+
+lamaStudi = input("Lama belajar          : ")
+lamaStudi = float(lamaStudi)
+
+print("HASIL CAPAIAN PREDIKAT LULUSAN S1 TAHUN 2021")
+print("Nama                : " + namaLengkap)
+print("NIM                 : " + nim)
+
+if (ipk >= 3.51 and lamaStudi <= 4):
+    print("Selamat! Anda berhasil lulus dengan predikat Pujian Tertinggi (Summa Cumlaude).")
+
+elif (ipk >= 3.51):
+    print("Selamat! Anda berhasil lulus dengan predikat pujian (Cumlaude).")
+
+elif (ipk >= 3.01 and ipk < 3.51):
+    print("Selamat! Anda berhasil lulus dengan predikat sangat memuaskan.")
+
+elif(ipk >= 2.76 and ipk < 3.01):
+    print("Selamat! Anda berhasil lulus dengan predikat memuaskan.")
+
+elif(ipk >= 2.00 and ipk < 2.76):
+    print("Selamat! Anda berhasil lulus dengan predikat cukup.")
+
+elif(ipk < 2.00):
+    print("Mohon maaf, anda dinyatakan tidak lulus, silahkan mengulang kembali pada periode berikutnya. ", end=" ")
+    print("Jangan putus asa dan tetap semangat!")
+
+else:
+    print("Nilai IPK yang anda masukkan tidak valid. Silahkan mencoba kembali ", end=" ")
+    print("dan pastikan nilai yang anda berikan sesuai format.")
